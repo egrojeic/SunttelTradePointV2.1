f1a380a57c5b91d100b70ef83a3053341b985401
egrojeic <35243991+egrojeic@users.noreply.github.com>
2023-02-21 11:17:26 -0500

    Update UIClientGlobalVariables with PublicIpAddress

--- SunttelTradePointB/Client/UIClientGlobalVariables.cs
+++ SunttelTradePointB/Client/UIClientGlobalVariables.cs
@@ -5,6 +5,6 @@
         public static string UserId { get; set; } = "";
         public static string UserName { get; set; } = "";
 
-
+        public static string PublicIpAddress { get; set; } = "";
     }
 }
