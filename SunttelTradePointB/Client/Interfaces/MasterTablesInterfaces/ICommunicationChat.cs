c6dfddfda7caadb1bba7a05630e10d9ff554f00c
DavidMoreo <webmigestion@gmail.com>
2023-03-14 15:07:48 -0600

    Ajuste

--- SunttelTradePointB/Client/Interfaces/MasterTablesInterfaces/ICommunicationChat.cs
+++ SunttelTradePointB/Client/Interfaces/MasterTablesInterfaces/ICommunicationChat.cs
@@ -8,5 +8,13 @@ namespace SunttelTradePointB.Client.Interfaces.MasterTablesInterfaces
         Task<List<ChannelCommunicationsGroup>> GetChannelCommunicationsGroups();
         Task<bool> SaveChannelCommunicationsGroup(ChannelCommunicationsGroup channelCommunicationsGroup);
 
+        /// <summary>
+        /// Retrives a list with CommunicationsMessage Items meeting search criteria
+        /// </summary>
+        /// <param name="channelCommunicationGroupId"></param>
+        /// <param name="startingDate"></param>
+        /// <param name="filterCriteria"></param>       
+        /// <returns></returns>
+        Task<List<CommunicationsMessage>> GetMessagesOfAnEntity(string channelCommunicationGroupId, string? startingDate = null, string? filterCriteria = null);
     }
 }
