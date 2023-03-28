ed8fa5b0edfd4d22d3794180f15aa38d2ba14580
juankmanca <juankmanca@gmail.com>
2023-03-28 10:25:15 -0500

    cambio a IDSquad

--- SunttelTradePointB/Client/Services/MasterTablesServices/ActorsNodeService.cs
+++ SunttelTradePointB/Client/Services/MasterTablesServices/ActorsNodeService.cs
@@ -1040,7 +1040,7 @@ namespace SunttelTradePointB.Client.Services.MasterTablesServices
             try
             {
                 var request = new HttpRequestMessage(HttpMethod.Get, Url);
-                request.Headers.Add("SquadId", UIClientGlobalVariables.ActiveSquad.ID.ToString());
+                request.Headers.Add("SquadId", UIClientGlobalVariables.ActiveSquad.IDSquads.ToString());
 
 
                 var response = await _httpClient.SendAsync(request);
