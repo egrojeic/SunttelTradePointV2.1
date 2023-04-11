48e0b68b143123b645204581628a748e7c8f3046
DavidMoreo <webmigestion@gmail.com>
2023-04-11 16:46:24 -0600

    ajuste speak

--- SunttelTradePointB/Client/wwwroot/js/speech.js
+++ SunttelTradePointB/Client/wwwroot/js/speech.js
@@ -6,9 +6,36 @@ window.ChatScroll = function () {
 
 
 
+
+var voiceConfig = {
+    "name": "MiVozPersonalizada",
+    "lang": "es-ES",
+    "localService": true,
+    "voiceURI": "MiVozPersonalizada",
+    "gender": "female",
+    "localService": true,
+    "default": false,
+    "extensions": {
+        "custom-voice": {
+            "wav": ["ruta/al/archivo/de/audio1.wav", "ruta/al/archivo/de/audio2.wav"],
+            "metadata": {
+                "voice": "MiVozPersonalizada",
+                "description": "Mi descripción personalizada"
+            }
+        }
+    }
+};
+
+
 window.speak = function (value) {
-    var text = new SpeechSynthesisUtterance(value);
-    speechSynthesis.speak(text);
+    var synthesisUtterance = new SpeechSynthesisUtterance(value);
+    synthesisUtterance.text = value;
+    synthesisUtterance.rate =1;
+    synthesisUtterance.pitch =1;
+    synthesisUtterance.volume = 8;
+    synthesisUtterance.lang = "en-US";  
+    
+    speechSynthesis.speak(synthesisUtterance);
 }
 
 
