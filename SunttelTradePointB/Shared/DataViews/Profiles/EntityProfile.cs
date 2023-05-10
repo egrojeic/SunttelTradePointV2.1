46ec8028b9277edd43919b2ca41cc12a3369fa99
CristianCamilo <tiancamilo59@gmail.com>
2023-05-10 13:11:11 -0500

    Ajustando servicio SaveEntitiesCSV

---
+++ SunttelTradePointB/Shared/DataViews/Profiles/EntityProfile.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using SunttelTradePointB.Shared.Common;
+using SunttelTradePointB.Shared.DataViews.BI;
+using SunttelTradePointB.Shared.ImportingData;
+using SunttelTradePointB.Shared.Sales;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SunttelTradePointB.Shared.DataViews.Profiles
+{
+    internal class EntityProfile : Profile
+    {
+        public EntityProfile() 
+        {
+            CreateMap<ActorsImport, EntityActor>();
+                
+        }
+    }
+}
