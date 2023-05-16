b19430280bd341ab996a2d6ec6f37be4e9885894
juankmanca <juankmanca@gmail.com>
2023-05-16 17:01:26 -0500

    Creacion de index menu

--- SunttelTPointVendingSite/Program.cs
+++ SunttelTPointVendingSite/Program.cs
@@ -1,3 +1,4 @@
+using CurrieTechnologies.Razor.SweetAlert2;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using SunttelTPointVendingSite;
@@ -7,5 +8,6 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddSweetAlert2();
 
 await builder.Build().RunAsync();
