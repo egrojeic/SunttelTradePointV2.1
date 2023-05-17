5e16035a80cf05103001cd91b21f587ce6b958ff
juankmanca <juankmanca@gmail.com>
2023-05-17 17:00:01 -0500

    avances vista principal vendingSite

--- SunttelTPointVendingSite/Program.cs
+++ SunttelTPointVendingSite/Program.cs
@@ -1,3 +1,4 @@
+using Blazored.Modal;
 using CurrieTechnologies.Razor.SweetAlert2;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -7,7 +8,10 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddBlazoredModal();
+builder.Services.AddAuthorizationCore();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddSweetAlert2();
 
 await builder.Build().RunAsync();
+
